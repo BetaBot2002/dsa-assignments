e455e7707b05911a2a1803d1085bc2891c42c92c
Sinchan Nandy <105705266+BetaBot2002@users.noreply.github.com>
2023-11-14 16:11:47 +0530

    Intersection Updated

--- IntersectionOfTwoArray.c
+++ IntersectionOfTwoArray.c
@@ -19,23 +19,35 @@ void findIntersection(int* arr1,int size1, int* arr2,int size2){
 }
 
 int main(){
-    int arr[5];
-    int size = sizeof(arr) / sizeof(arr[0]);
+    int size1;
+    printf("Enter the size of array:\n");
+    scanf("%d",&size1);
+
+    int *arr1=(int*)malloc(size1*sizeof(int));
 
     printf("Enter the array elements:\n");
-    for (int i = 0; i < size; i++){
-        scanf("%d", &arr[i]);
+    for (int i = 0; i < size1; i++){
+       scanf("%d",&arr1[i]);
     }
+    printf("The array elements are:\n");
+    for (int i = 0; i < size1; i++){
+       printf("%d\n",arr1[i]);
+    }
+    int size2;
+    printf("Enter the size of array:\n");
+    scanf("%d",&size2);
 
-    int arr2[5];
-    int size2 = sizeof(arr) / sizeof(arr[0]);
+    int *arr2=(int*)malloc(size2*sizeof(int));
 
-    printf("Enter the array2 elements:\n");
+    printf("Enter the array elements:\n");
     for (int i = 0; i < size2; i++){
-        scanf("%d", &arr2[i]);
+       scanf("%d",&arr2[i]);
     }
-
-    findIntersection(arr,size,arr2,size2);
+    printf("The array elements are:\n");
+    for (int i = 0; i < size2; i++){
+       printf("%d\n",arr2[i]);
+    }    
+    findIntersection(arr1,size1,arr2,size2);
 
     return 0;
 }
