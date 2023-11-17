205183b7dcf8360edaf385ad8d4a3f698ad6c617
Sinchan Nandy <105705266+BetaBot2002@users.noreply.github.com>
2023-11-18 02:33:39 +0530

    Delete Node added

---
+++ DeleteNodeLinkedList.c
@@ -0,0 +1,68 @@
+// Assignment 15
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node{
+    int data;
+    struct Node* next;
+} Node;
+
+Node* createNode(int data){
+    Node* newNode=(Node*)malloc(sizeof(Node));
+    newNode->data=data;
+    newNode->next=NULL;
+    return newNode;
+}
+
+Node* insertNode(Node* head,int data){
+    if(head==NULL) return createNode(data);
+
+    Node* temp=head;
+    head=createNode(data);
+    head->next=temp;
+    
+    return head;
+}
+
+Node* deleteNode(Node* head,int data){
+    Node* temp=head;
+    while(temp->next->data!=data){
+        temp=temp->next;
+        if(temp->next == NULL) return NULL;
+    }
+    Node* deleted=temp->next;
+    temp->next=deleted->next;
+    return deleted;
+}
+
+void displayList(Node* head){
+    Node* current=head;
+    while (current != NULL){
+        current->next==NULL?printf("%d",current->data):printf("%d->",current->data);
+        current=current->next;
+    }
+    printf("\n");
+}
+
+
+int main(){
+    Node* LinkedList=NULL;
+    LinkedList=insertNode(LinkedList,1);
+    LinkedList=insertNode(LinkedList,2);
+    LinkedList=insertNode(LinkedList,3);
+    LinkedList=insertNode(LinkedList,4);
+
+    displayList(LinkedList);
+
+    Node* deleted=deleteNode(LinkedList,2);
+
+    if(deleted!=NULL){
+        printf("Deleted: %d\n",deleted->data);
+        printf("The Linked List after deltetion:\n");
+        displayList(LinkedList);
+    }else{
+        printf("Node Not Found\n");
+    }
+
+    return 0;
+}
